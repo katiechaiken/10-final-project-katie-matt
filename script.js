cd01890b3e3f591cd61a5ebd09e2fc4ee3387289
katiechaiken <katie.chaiken@wustl.edu>
2019-12-08 15:36:21 -0600

    get user input

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-
-// ARTIST INFO
+// PULL ARTIST INFO THROUGH USER INPUT!
 $(document).ready(function(){
-  $.ajax({
-    type: 'POST',
-    url: 'http://ws.audioscrobbler.com/2.0/',
-    data: 'method=artist.getinfo&' +
-    'artist=Jon+Bellion&' +
-    'api_key=57ee3318536b23ee81d6b27e36997cde&' +
-    'format=json',
-    dataType: 'jsonp',
-    success: function(data) {
-      $('#success #artistName').html(data.artist.name);
-      $('#success #artistBio').html(data.artist.bio.content);
-    },
+  var artist;
+
+  $("#enterbutton").click(function(){
+    artist = ($('#input').val())
+
+    for(var i = 0; i < artist.length; i++){
+      artist = artist.replace(" ", "+");
+    }
+      $.ajax({
+        type: 'POST',
+        url: 'http://ws.audioscrobbler.com/2.0/',
+        data: 'method=artist.getinfo&' +
+        'artist=' + artist + '&' +
+        'api_key=57ee3318536b23ee81d6b27e36997cde&' +
+        'format=json',
+        dataType: 'jsonp',
+        success: function(data) {
+          $('#success #artistName').html(data.artist.name);
+          $('#success #artistBio').html(data.artist.bio.content);
+        },
+      });
   });
+
 });
 
 // ALBUM INFO
 $(document).ready(function(){
+  var artist;
+  var album;
+  $("#enterbutton").click(function(){
+    artist = ($('#input').val())
+    album = ($('#input2').val())
+    for(var i = 0; i < artist.length; i++){
+      artist = artist.replace(" ", "+");
+    }
+    for(var i = 0; i < album.length; i++){
+      album = album.replace(" ", "+");
+    }
+
   $.ajax({
     type: 'POST',
     url: 'http://ws.audioscrobbler.com/2.0/',
     data: 'method=album.getinfo&' +
-    'artist=Jon+Bellion&' +
-    'album=The+Human+Condition&' +
+    'artist=' + artist + '&' +
+    'album=' + album + '&' +
     'api_key=57ee3318536b23ee81d6b27e36997cde&' +
     'format=json',
     dataType: 'jsonp',
@@ -42,14 +63,23 @@ $(document).ready(function(){
     }
   });
 });
+});
 
 //TOP TRACKS
 $(document).ready(function(){
+  var artist;
+
+  $("#enterbutton").click(function(){
+    artist = ($('#input').val())
+
+    for(var i = 0; i < artist.length; i++){
+      artist = artist.replace(" ", "+");
+    }
   $.ajax({
     type: 'POST',
     url: 'http://ws.audioscrobbler.com/2.0/',
     data: 'method=artist.gettoptracks&' +
-    'artist=Jon+Bellion&' +
+    'artist=' + artist + '&' +
     'limit=10&' +
     'api_key=57ee3318536b23ee81d6b27e36997cde&' +
     'format=json',
@@ -70,15 +100,23 @@ $(document).ready(function(){
     }
   });
 });
+});
 
 var songID;
 //PULL ARTIST IMAGE AND SONG ID AND THEN GET TRACK ID TO PLAY
 $(document).ready(function(){
+  var artist;
 
+  $("#enterbutton").click(function(){
+    artist = ($('#input').val())
+
+    for(var i = 0; i < artist.length; i++){
+      artist = artist.replace(" ", "%20");
+    }
   var artistAPI = {
     "async": true,
     "crossDomain": true,
-    "url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=jon%20bellion",
+    "url": "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist,
     "method": "GET",
     "headers": {
       "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
@@ -93,6 +131,8 @@ $(document).ready(function(){
 
     //SONG STUFF
     songID = data.data[0]["id"];
+    //to get songs from album for more specific plays!
+    //https://rapidapi.com/deezerdevs/api/deezer-1?endpoint=53aa5085e4b07e1f4ebeb42c
     var trackAPI = {
       "async": true,
       "crossDomain": true,
@@ -106,7 +146,7 @@ $(document).ready(function(){
     $.ajax(trackAPI).done(function (data) {
       console.log(data);
       console.log(data.preview);
-      var song = document.createElement("audio");
+      var song = document.getElementById("player");
       song.src = data.preview;
       $("#play").click(function(){
         song.play();
@@ -117,5 +157,6 @@ $(document).ready(function(){
     });
 
   });
+});
 
 });
