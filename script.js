0751dd41d377faf2535b1ebe21f5cfe6f6735718
Matt Lopes <matthew.lopes@wustl.edu>
2019-12-15 22:39:57 -0600

    artist css

--- script.js
+++ script.js
@@ -318,10 +318,12 @@ function cssSelector(){
     css.href = "file:///Users/kachaiken/Dropbox/cse204/10-final-project-katie-matt/artist.css";
     document.getElementById("albumTracks").innerHTML = "";
     document.getElementById("albumImage").innerHTML = "";
+    document.getElementById('header').innerHTML = "Artist";
   }
   else if(($('#albuminput').val()).length > 0){
     var css = document.getElementById("style2");
     css.href = "file:///Users/kachaiken/Dropbox/cse204/10-final-project-katie-matt/artistalbum.css";
+    document.getElementById('header').innerHTML = "Artist and Album";
   }
 }
 $("#enterbutton").click(function () {
