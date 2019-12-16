9751b0edb5495994118f56ff6869d53ef9cffd22
katiechaiken <katie.chaiken@wustl.edu>
2019-12-15 21:23:13 -0600

    commit

--- homescript.js
+++ homescript.js
@@ -4,22 +4,22 @@
       global.localStorage.setItem("sharedAlbum", document.getElementById("homealbum").value);
 global.localStorage.setItem("clickcount", 1);
 }, false);
-document.getElementById("JonBellion").addEventListener("click", function () {
-global.localStorage.setItem("sharedArtist", "Jon Bellion");
-global.localStorage.setItem("clickcount", 1);
-}, false);
-document.getElementById("TaylorSwift").addEventListener("click", function () {
-global.localStorage.setItem("sharedArtist", "Taylor Swift");
-global.localStorage.setItem("clickcount", 1);
-}, false);
-document.getElementById("TheBeatles").addEventListener("click", function () {
-global.localStorage.setItem("sharedArtist", "The Beatles");
-global.localStorage.setItem("clickcount", 1);
-}, false);
-document.getElementById("JayZ").addEventListener("click", function () {
-global.localStorage.setItem("sharedArtist", "Jay-Z");
-global.localStorage.setItem("clickcount", 1);
-}, false);
+// document.getElementById("JonBellion").addEventListener("click", function () {
+// global.localStorage.setItem("sharedArtist", "Jon Bellion");
+// global.localStorage.setItem("clickcount", 1);
+// }, false);
+// document.getElementById("TaylorSwift").addEventListener("click", function () {
+// global.localStorage.setItem("sharedArtist", "Taylor Swift");
+// global.localStorage.setItem("clickcount", 1);
+// }, false);
+// document.getElementById("TheBeatles").addEventListener("click", function () {
+// global.localStorage.setItem("sharedArtist", "The Beatles");
+// global.localStorage.setItem("clickcount", 1);
+// }, false);
+// document.getElementById("JayZ").addEventListener("click", function () {
+// global.localStorage.setItem("sharedArtist", "Jay-Z");
+// global.localStorage.setItem("clickcount", 1);
+// }, false);
 }(window));
 
 var song = new Audio('shewillbeloved.mp3');
